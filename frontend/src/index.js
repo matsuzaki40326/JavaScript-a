686c84026329b59c21c791fd14b8bf66d27f97bb
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-16 18:23:32 +0900

    91

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,26 @@
 import './style.scss'
 console.log('webpackテストです');
+require('intersection-observer');
 
-window.addEventListener('load', () => {
-  alert('babelテスト');
-})
+// window.addEventListener('load', () => {
+//   alert('babelテスト');
+// })
+
+const options = {
+  threshold:'0.5',
+  rootMargin:'-200px'
+};
+const observer = new IntersectionObserver(callback,options);
+const targets = document.querySelectorAll('.message-box');
+targets.forEach((target) => {
+  observer.observe(target);
+});
+
+function callback(entries){
+  entries.forEach(entry => {
+    console.log(entry);
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+}
