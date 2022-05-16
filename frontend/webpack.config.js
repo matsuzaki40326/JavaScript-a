e502d6633e0ac4e483d2427bd1751a9275962ffb
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-16 17:30:43 +0900

    86

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,4 +6,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: ['style-loader','css-loader'] //後ろから実行される
+      }
+    ],
+  },
 };
